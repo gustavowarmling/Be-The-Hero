200e9556b79ebde02d865f3ca60fc80c9db2658d
Gustavo Warmlinh <gustavowarmling@gmail.com>
2020-03-24 21:41:34 -0300

    Api da aplicação

---
+++ api/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const Ongcontroller = require('./controllers/Ongcontroller');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', Ongcontroller.index);
+routes.post('/ongs', Ongcontroller.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
